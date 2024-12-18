dd56e4b55d88d4a48eb2162ef46575ada7eee564
Chase Feng <chase.feng@gmail.com>
2024-12-18 07:04:39 -0500

    Parser tests now working.

--- src-test/tests.ml
+++ src-test/tests.ml
@@ -116,93 +116,95 @@ module Lexer_tests =
   end
 
   module Parser_tests =
-  struct
-    module S = Cocaml.Syntax_node
-    module M = Cocaml_main
-
-    (* Read a file into a string *)
-    let read_file (filename: string) : string = 
-      In_channel.read_all filename
-
-    (* Write a string to a file *)
-    let write_file (filename: string) (content: string) =
-      Out_channel.with_file filename ~f:(fun out_channel ->
-        Out_channel.output_string out_channel content)
-  
-    (* Convert a Syntax_node.prog to an S-expression string *)
-    let prog_to_sexp (prog: S.prog) : string = 
-      S.sexp_of_prog ast |> Sexp.to_string_hum
-  
-    let test_parser_simple _ = 
-      let input_path = "../../../test/simple.c" in
-      let result = M.parse_c_to_ast input_path in
+    struct
+      module S = Cocaml.Syntax_node
+      module M = Cocaml_main
       
-      let output_path = "../../../test/simple_ast.txt" in
-      write_file output_path (prog_to_sexp result) in
-      assert_bool
-        "The simple AST should not be invalid!"
-        (not (S.equal_prog result (S.prog [])))
-
-    let test_parser_switch _ = 
-      let input_path = "../../../test/switch.c" in
-      let result = M.parse_c_to_ast input_path in
-      
-      let output_path = "../../../test/switch_ast.txt" in
-      write_file output_path (prog_to_sexp result) in
-      assert_bool
-        "The switch AST should not be invalid!"
-        (not (S.equal_prog result (S.prog [])))
-
-    let test_parser_loop _ = 
-      let input_path = "../../../test/loop.c" in
-      let result = M.parse_c_to_ast input_path in
-      
-      let output_path = "../../../test/loop_ast.txt" in
-      write_file output_path (prog_to_sexp result) in
-      assert_bool
-        "The loop AST should not be invalid!"
-        (not (S.equal_prog result (S.prog [])))
-
-    let test_parser_loop_undefined _ = 
-      let input_path = "../../../test/loop_undefined_variable.c" in
-      let result = M.parse_c_to_ast input_path in
-      
-      let output_path = "../../../test/loop_undefined_variable_ast.txt" in
-      write_file output_path (prog_to_sexp result) in
-      assert_bool
-        "The loop (with an undefined variable) AST should not be invalid!"
-        (not (S.equal_prog result (S.prog [])))
-
-    let test_parser_no_colon _ = 
-      let input_path = "../../../test/no_colon.c" in
-      let result = M.parse_c_to_ast input_path in
-      
-      let output_path = "../../../test/no_colon.txt" in
-      write_file output_path (prog_to_sexp result) in
-      assert_bool
-        "The colon-missing AST should not be valid!"
-        (S.equal_prog result (S.prog []))
+      (* Write a string to a file *)
+      let write_file (filename: string) (content: string) =
+        Out_channel.with_file filename ~f:(fun out_channel ->
+          Out_channel.output_string out_channel content)
     
-    let test_parser_full_c _ = 
-      let input_path = "../../../test/large.c" in
-      let result = M.parse_c_to_ast input_path in
+      (* Convert a Syntax_node.prog to an S-expression string *)
+      let prog_to_sexp (prog: S.prog) : string = 
+        S.sexp_of_prog prog |> Sexp.to_string_hum
+
+      (* Check if Syntax_node.prog is empty *)
+      let is_prog_empty (prog: S.prog) : bool =
+        match prog with
+          | S.Prog [] -> true
+          | _ -> false
+    
+      let test_parser_simple _ = 
+        let input_path = "../../../test/simple.c" in
+        let result = M.parse_c_to_ast input_path in
+        
+        let output_path = "../../../test/simple_ast.txt" in
+        write_file output_path (prog_to_sexp result);
+        assert_bool
+          "The simple AST should not be invalid!"
+          (not (is_prog_empty result))
+
+      let test_parser_switch _ = 
+        let input_path = "../../../test/switch.c" in
+        let result = M.parse_c_to_ast input_path in
+        
+        let output_path = "../../../test/switch_ast.txt" in
+        write_file output_path (prog_to_sexp result);
+        assert_bool
+          "The switch AST should not be invalid!"
+          (not (is_prog_empty result))
+
+      let test_parser_loop _ = 
+        let input_path = "../../../test/loop.c" in
+        let result = M.parse_c_to_ast input_path in
+        
+        let output_path = "../../../test/loop_ast.txt" in
+        write_file output_path (prog_to_sexp result);
+        assert_bool
+          "The loop AST should not be invalid!"
+          (not (is_prog_empty result))
+
+      let test_parser_loop_undefined _ = 
+        let input_path = "../../../test/loop_undefined_variable.c" in
+        let result = M.parse_c_to_ast input_path in
+        
+        let output_path = "../../../test/loop_undefined_variable_ast.txt" in
+        write_file output_path (prog_to_sexp result);
+        assert_bool
+          "The loop (with an undefined variable) AST should not be invalid!"
+          (not (is_prog_empty result))
+
+      let test_parser_no_colon _ = 
+        let input_path = "../../../test/no_colon.c" in
+        let result = M.parse_c_to_ast input_path in
+        
+        let output_path = "../../../test/no_colon.txt" in
+        write_file output_path (prog_to_sexp result);
+        assert_bool
+          "The colon-missing AST should not be valid!"
+          (is_prog_empty result)
       
-      let output_path = "../../../test/large.txt" in
-      write_file output_path (prog_to_sexp result) in
-      assert_bool
-        "The full c AST should not be invalid!"
-        (not (S.equal_prog result (S.prog [])))
-      
-    let series =
-      "Parser Tests" >::: [
-        "Parser Simple Test" >:: test_parser_simple;
-        "Parser Switch Test" >:: test_parser_switch;
-        "Parser Loop Test" >:: test_parser_loop;
-        "Parser Loop with Undefined Test" >:: test_parser_loop_undefined;
-        "Parser Missing Colon Test" >:: test_parser_no_colon;
-        "Parser Full C Test" >:: test_parser_full_c
-      ]
-  end
+      let test_parser_full_c _ = 
+        let input_path = "../../../test/large.c" in
+        let result = M.parse_c_to_ast input_path in
+        
+        let output_path = "../../../test/large.txt" in
+        write_file output_path (prog_to_sexp result);
+        assert_bool
+          "The full c AST should not be invalid!"
+          (not (is_prog_empty result))
+        
+      let series =
+        "Parser Tests" >::: [
+          "Parser Simple Test" >:: test_parser_simple;
+          "Parser Switch Test" >:: test_parser_switch;
+          "Parser Loop Test" >:: test_parser_loop;
+          "Parser Loop with Undefined Test" >:: test_parser_loop_undefined;
+          "Parser Missing Colon Test" >:: test_parser_no_colon;
+          "Parser Full C Test" >:: test_parser_full_c
+        ]
+    end
 
 module Translator_tests =
   struct
